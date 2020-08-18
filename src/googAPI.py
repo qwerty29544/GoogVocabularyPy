ea864319c618970d91659fdc88a690a6f7872502
qwerty29544 <qwerty29544@gmail.com>
2020-08-18 21:13:39 +0300

    Translate words function with googletrans api

--- src/googAPI.py
+++ src/googAPI.py
@@ -1,7 +1,13 @@
 import googletrans
 
-translator = googletrans.Translator()
-textesr= ["geography", "Plain", "text", "groggy main tank"]
-for i in textesr:
-    print(translator.translate(text=i, src='en', dest='ru'))
-print(translator.detect("%@#!@#@$!@$!@$!@$"))
+
+def translate_word(*word, from_lang="en", to_lang="ru"):
+    translator = googletrans.Translator()
+    word_in_lang = []
+    for single_word in word:
+        word_translation = translator.translate(text=str(single_word), src=from_lang, dest=to_lang)
+        word_in_lang.append(word_translation.text)
+    return word_in_lang[0]
+
+
+print(translate_word("today is your birthday"))
